9e1069a5075987771c6100fd96f64a1e86cc61b9
root <root@LAPTOP-UFAB25UP>
2023-08-18 00:25:09 +0800

    lab6

--- user/uthread.c
+++ user/uthread.c
@@ -10,11 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+/* we must defind it before defining thread */
+/* otherwise, we will meet "incomplete type is not allowed" */
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;       /* Saved registers for user context switches. */
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
@@ -58,10 +80,8 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+
+    thread_switch((uint64)&t->context, (uint64)&next_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,6 +96,12 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  // from kernel/proc.c/allocproc(), we know we need get thread's address of ret and thread's stack pointer
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)(t->stack + STACK_SIZE);
+  // I know we need stack to store state, but how to use it to store 'state' or other? (over)
+  // the C compiler generates code in the caller to save caller-saved registers on the stack.
+  // so we know state can be store as a caller register in thread own's stack[STACK_SIZE] 
 }
 
 void 
